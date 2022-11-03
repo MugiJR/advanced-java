66cd59c12d549dcc33931e7e2ee3ea03123bfccc
MugiJR <immugi@yahoo.com>
2022-11-03 17:47:54 +0100

    Added serialization package

---
+++ src/serialization/SaveToFile.java
@@ -0,0 +1,21 @@
+package serialization;
+
+import java.io.*;
+
+public class SaveToFile {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        try(var oos = new ObjectOutputStream(new FileOutputStream("File.dat"))) {
+            oos.writeObject(1212);
+            oos.writeObject("hello world from serialization");
+        }
+
+        try(var ois = new ObjectInputStream(new FileInputStream("File.dat"))) {
+            int num = (Integer)ois.readObject();
+            String line = (String)ois.readObject();
+
+            System.out.println("Then number is  "+num+" and line is "+line);
+
+        }
+    }
+}
